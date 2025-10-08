11842e9e900782f253b86d7e49b25b9a80d98406
Arifdamri01 <arif_t@oppstar.com.my>
2025-10-08 14:21:25 +0800

    push

--- q1_basic_float_pointer/basic.c
+++ q1_basic_float_pointer/basic.c
@@ -28,7 +28,7 @@ int main() {
     printf ("Value at p (*p): %.1f\n", *p);
     *p = 30.0f;
     printf ("New value of x: %.1f\n", x);
-    
+    printf ("Correct implementation!\n");
     
     return 0;
 }
